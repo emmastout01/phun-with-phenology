9bdd5e835aa4f3bd9a3680b47bed9313dba178a2
Emma Stout <emma@primeacademy.io>
2023-04-20 15:36:25 -0500

    add db setup, add boilerplate server routes

--- database.sql
+++ database.sql
@@ -8,3 +8,25 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "note" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INTEGER REFERENCES "user",
+    "date" DATE,
+    "time" VARCHAR (40),
+    "location" VARCHAR (1000),
+    "content" TEXT
+    );
+    
+CREATE TABLE "bird" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (500),
+    "photo" VARCHAR (2000)
+    );
+    
+CREATE TABLE "bird_note" (
+    "id" SERIAL PRIMARY KEY,
+    "bird_id" INTEGER REFERENCES "bird",
+    "note_id" INTEGER REFERENCES "note", 
+    "content" TEXT
+    );
